c5a3889df3dd4700e9def8392ad2848ce6170004
ValentinSchmid <45970251+ValentinSchmid@users.noreply.github.com>
2019-03-17 17:10:28 +0100

    neues reward und größeres Q

--- callbacks.py
+++ callbacks.py
@@ -1,155 +1,91 @@
-
 import numpy as np
+import random
 from random import shuffle
 from time import time, sleep
 from collections import deque
+import os.path as op
 
 from settings import s
+from settings import e
 
-
-def look_for_targets(free_space, start, targets, logger=None):
-    """Find direction of closest target that can be reached via free tiles.
-
-    Performs a breadth-first search of the reachable free tiles until a target is encountered.
-    If no target can be reached, the path that takes the agent closest to any target is chosen.
-
-    Args:
-        free_space: Boolean numpy array. True for free tiles and False for obstacles.
-        start: the coordinate from which to begin the search.
-        targets: list or array holding the coordinates of all target tiles.
-        logger: optional logger object for debugging.
-    Returns:
-        coordinate of first step towards closest target or towards tile closest to any target.
-    """
-    if len(targets) == 0: return None
-
-    frontier = [start]
-    parent_dict = {start: start}
-    dist_so_far = {start: 0}
-    best = start
-    best_dist = np.sum(np.abs(np.subtract(targets, start)), axis=1).min()
-
-    while len(frontier) > 0:
-        current = frontier.pop(0)
-        # Find distance from current position to all targets, track closest
-        d = np.sum(np.abs(np.subtract(targets, current)), axis=1).min()
-        if d + dist_so_far[current] <= best_dist:
-            best = current
-            best_dist = d + dist_so_far[current]
-        if d == 0:
-            # Found path to a target's exact position, mission accomplished!
-            best = current
-            break
-        # Add unexplored free neighboring tiles to the queue in a random order
-        x, y = current
-        neighbors = [(x,y) for (x,y) in [(x+1,y), (x-1,y), (x,y+1), (x,y-1)] if free_space[x,y]]
-        shuffle(neighbors)
-        for neighbor in neighbors:
-            if neighbor not in parent_dict:
-                frontier.append(neighbor)
-                parent_dict[neighbor] = current
-                dist_so_far[neighbor] = dist_so_far[current] + 1
-    if logger: logger.debug(f'Suitable target found at {best}')
-    # Determine the first step towards the best found target tile
-    current = best
-    while True:
-        if parent_dict[current] == start: return current
-        current = parent_dict[current]
-
+def statefun(arena,agent_row,agent_col,coins):    
+    accessible = []
+    for a in range(16):
+        for b in range(16):
+            if (arena[a][b] != -1):
+                accessible.append((a,b))    
+    state=accessible.index((agent_row,agent_col))
+    for i in range(len(coins)):
+        state = state + accessible.index((coins[i][0],coins[i][1]))
+    return state
 
 def setup(self):
-    """Called once before a set of games to initialize data structures etc.
-
-    The 'self' object passed to this method will be the same in all other
-    callback methods. You can assign new properties (like bomb_history below)
-    here or later on and they will be persistent even across multiple games.
-    You can also use the self.logger object at any time to write to the log
-    file for debugging (see https://docs.python.org/3.7/library/logging.html).
-    """
-    self.logger.debug('Successfully entered setup code')
-    np.random.seed()
-    # Fixed length FIFO queues to avoid repeating the same actions
-    self.bomb_history = deque([], 5)
+    np.random.seed()    
+    # Q matrix
+    if op.isfile("Q.txt") != True:        
+        Q = np.zeros((176*9,5),dtype = float)
+        np.savetxt("Q.txt", Q)
     self.coordinate_history = deque([], 20)
-    # While this timer is positive, agent will not hunt/attack opponents
-    self.ignore_others_timer = 0
-
-
-def act(self):
-    """Called each game step to determine the agent's next action.
-
-    You can find out about the state of the game environment via self.game_state,
-    which is a dictionary. Consult 'get_state_for_agent' in environment.py to see
-    what it contains.
-
-    Set the action you wish to perform by assigning the relevant string to
-    self.next_action. You can assign to this variable multiple times during
-    your computations. If this method takes longer than the time limit specified
-    in settings.py, execution is interrupted by the game and the current value
-    of self.next_action will be used. The default value is 'WAIT'.
-    """
-    self.logger.info('Picking action according to rule set')
-
-    #Game state
+    self.logger.info('Initialize')
+def act(self):    
+    Q = np.loadtxt("Q.txt")
     arena = self.game_state['arena']
-    x, y, _, bombs_left, score = self.game_state['self']
     coins = self.game_state['coins']
+    x, y, _, bombs_left, score = self.game_state['self']
+    self.coordinate_history.append((x,y))
+    state = (x,y)    
+    epsilon = 0.7
+    action_ideas = ['UP', 'DOWN', 'LEFT', 'RIGHT','WAIT']
+    shuffle(action_ideas)        
+    if np.random.rand(1) <= epsilon:
+        self.next_action = action_ideas.pop()
+    else:
+        index = statefun(arena,x,y,coins)
+        q_state = Q[index]
+        act = np.argmax(q_state)
+        
+        if act == 0: action_ideas.append('UP')
+        if act == 1: action_ideas.append('DOWN')
+        if act == 2: action_ideas.append('LEFT')
+        if act == 3: action_ideas.append('RIGHT')
+        if act == 4: action_ideas.append('WAIT')        
+        self.next_action = action_ideas.pop()
+    self.logger.info('Pick action at random')
 
-    # Check which moves make sense at all
-    directions = [(x,y), (x+1,y), (x-1,y), (x,y+1), (x,y-1)]
-    valid_tiles, valid_actions = [], []
-    for d in directions:
-        if ((arena[d] == 0)):
-            valid_tiles.append(d)
-    if (x-1,y) in valid_tiles: valid_actions.append('LEFT')
-    if (x+1,y) in valid_tiles: valid_actions.append('RIGHT')
-    if (x,y-1) in valid_tiles: valid_actions.append('UP')
-    if (x,y+1) in valid_tiles: valid_actions.append('DOWN')
-    if (x,y)   in valid_tiles: valid_actions.append('WAIT')
-
-    # Collect basic action proposals in a queue
-    # Later on, the last added action that is also valid will be chosen
-    action_ideas = ['UP', 'DOWN', 'LEFT', 'RIGHT']
-    shuffle(action_ideas) 
-
-    # Step towards next target
-    free_space = arena == 0
-    d = look_for_targets(free_space, (x,y), coins, self.logger)
-    if d == (x,y-1): action_ideas.append('UP')
-    if d == (x,y+1): action_ideas.append('DOWN')
-    if d == (x-1,y): action_ideas.append('LEFT')
-    if d == (x+1,y): action_ideas.append('RIGHT')
-    if d is None:
-        self.logger.debug('All targets gone, nothing to do anymore')
-        action_ideas.append('WAIT')
-
-
-    # Pick last action added to the proposals list that is also valid
-    while len(action_ideas) > 0:
-        a = action_ideas.pop()
-        if a in valid_actions:
-            self.next_action = a
-            break
-
-
-
-def reward_update(self):
-    """Called once per step to allow intermediate rewards based on game events.
-
-    When this method is called, self.events will contain a list of all game
-    events relevant to your agent that occured during the previous step. Consult
-    settings.py to see what events are tracked. You can hand out rewards to your
-    agent based on these events and your knowledge of the (new) game state. In
-    contrast to act, this method has no time limit.
-    """
-    self.logger.debug(f'Encountered {len(self.events)} game event(s)')
-
-
-def end_of_episode(self):
-    """Called at the end of each game to hand out final rewards and do training.
-
-    This is similar to reward_update, except it is only called at the end of a
-    game. self.events will contain all events that occured during your agent's
-    final step. You should place your actual learning code in this method.
-    """
+def reward_update(self):   
+    alpha = 1   
+    Q = np.loadtxt("Q.txt")
+    arena = self.game_state['arena']
+    coins = self.game_state['coins']
+    x, y, _, bombs_left, score = self.game_state['self']
+    state = statefun(arena,x,y,coins)
+    index = 4
+    
+    if self.events[0] == e.MOVED_LEFT: index=0
+    if self.events[0] == e.MOVED_RIGHT: index=1
+    if self.events[0] == e.MOVED_UP: index=2
+    if self.events[0] == e.MOVED_DOWN: index=3
+    if self.events[0] == e.WAITED: index=4
+    if len(self.events)>1:
+        if self.events[1] == e.COIN_COLLECTED:
+            Q[state][index]=Q[state][index]+alpha*100
+    else:
+            Q[state][index]=Q[state][index]-alpha*1
+    
+    np.savetxt("Q.txt", Q)    
+    
+    
+    
+def end_of_episode(self):  
+    Q = np.loadtxt("Q.txt")
+    alpha = 1
+    gamma = 0.9    
+   # for state in accessible:
+   #     if self.next_action == 'UP': next_state = (x,y+1)
+   #     if self.next_action == 'DOWN': next_state =  (x,y-1)
+   #     if self.next_action == 'LEFT': next_state =  (x-1,y)
+   #     if self.next_action == 'RIGHT': next_state = (x+1,y)
+   #     if self.next_action == 'WAIT': next_state = (x,y)
+   #     max_Q_next = np.argmax(Q[next_state])    
+   # Q[state][self.next_action] = Q[state][self.next_action] + alpha * (reward[state][self.next_action] + gamma * max_Q_next - Q[state][self.next_action])  
     self.logger.debug(f'Encountered {len(self.events)} game event(s) in final step')
